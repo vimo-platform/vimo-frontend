5ac601378e654dd6b5befc03b8b0e246c18571a4
온준규 <ojh6230@naver.com>
2026-08-08 02:03:18 +0900

    Expo SDK 54 호환 버전 구성

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,5 @@
-import { DarkTheme, DefaultTheme, Stack, ThemeProvider } from 'expo-router';
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { Platform, useColorScheme } from 'react-native';
 import { AnimatedSplashOverlay } from '@/features/auth/components/AnimatedSplashOverlay';
